25493c8078d7d8f1352423a82d94fff8ed4eb35d
Tamara Trefilova <tamara.trefilova@gmail.com>
2017-10-11 22:39:01 -0700

    Adding a file

--- src/FindMax.java
+++ src/FindMax.java
@@ -1,12 +1,26 @@
 
 public class FindMax {
-
 	/*
 	 * Define a program that finds the maximum number when 2 numbers are given.
 	 */
+	
+	/**
+	 * Returns max number
+	 * @param a - integer
+	 * @param b - integer
+	 */
+	public static void findMax(int a, int b) {
+		
+		if(a>b) {
+			System.out.println("Max number is: "+ a);
+		} else {
+			System.out.println("Max number is: "+ b);
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		findMax(10, -6);
 	}
 
 }
